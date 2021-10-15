e2a6a7bd12d199c669391ace73cebb2c5656fb2d
dsolerh <dsolerh.cinter95@gmail.com>
2021-10-14 21:00:51 -0400

    delete app.service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TaskModule } from './modules/tasks/tasks.module';
 import { AuthModule } from './modules/auth/auth.module';
@@ -9,6 +8,5 @@ import { AuthModule } from './modules/auth/auth.module';
 @Module({
   imports: [TypeOrmModule.forRoot(typeOrmConfig), TaskModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
